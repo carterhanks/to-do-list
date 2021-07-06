7564217cca83133979128a7e985791188ff7da14
carterhanks <carterhanks@gmail.com>
2021-07-05 21:42:46 -0600

    Changed Code Layout, Fixed CSS, Needs Cross Off Feature

--- public/index.js
+++ public/index.js
@@ -1,42 +1,66 @@
+// let addToDoButton = document.getElementById('addToDo');
+// let toDoContainer = document.getElementById('toDoContainer');
+// let inputField = document.getElementById('inputField');
 
-let addToDoButton = document.getElementById('addToDo');
-let toDoContainer = document.getElementById('toDoContainer');
-let inputField = document.getElementById('inputField');
+// addToDoButton.addEventListener('click', function(){
+//     let item = document.createElement('li');
 
-addToDoButton.addEventListener('click', function(){
-    let item = document.createElement('li');
+//     item.classList.add('paragraph-styling');
+//     item.innerText = inputField.value;
+//     toDoContainer.appendChild(item);
+//     inputField.value = "";
 
-    item.classList.add('paragraph-styling');
-    item.innerText = inputField.value;
-    toDoContainer.appendChild(item);
-    inputField.value = "";
+//     alert("Item Added!");
 
-    alert("Item Added!");
+//     item.addEventListener('click', function(){
+//         item.style.textDecoration = "line-through";
+//         onClick(alert("Item Completed!"));
+//     });
 
-    item.addEventListener('click', function(){
-        item.style.textDecoration = "line-through";
-        alert("Item Completed!");
-    });
+//     item.addEventListener('dblclick', function(){
+//         toDoContainer.removeChild(item);
+//         onDblClick(alert("Item Removed!"));
+//     });
+// });
 
-    item.addEventListener('dblclick', function(){
-        toDoContainer.removeChild(item);
-        alert("Item Removed!");
-    });
-});
 
-// document.querySelector("input").addEventListener("submit", addItem) 
-// function addItem(evt) {
-//     evt.preventDefault() {
-//         let inputField = document.querySelector("input");
+const message = document.querySelector("#message")
 
-//         const item = document.createElement("li");
+const addBtn = document.querySelector("form")
 
-//         const itemName = document.createElement("span");
-//             itemName.textContent = inputField.value;
-//             item.appendChild(itemName);
+function addItem(evt) {
+    evt.preventDefault();
+    const inputField = document.querySelector("input");
 
-//         const list = document.querySelector("ul");
-//             list.appendChild(item);
-//             inputField.value = ''
-//     }
-// }
+    const item = document.createElement("li");
+
+    const itemTitle = document.createElement("span");
+    itemTitle.textContent = inputField.value;
+    item.appendChild(itemTitle);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = ("Delete");
+    deleteBtn.addEventListener('click', deleteItem);
+    document.querySelector("ul").appendChild(item);
+    item.appendChild(deleteBtn);
+
+    inputField.value = '';
+};
+
+addBtn.addEventListener("submit", addItem);
+
+function deleteItem(evt) {
+    evt.preventDefault();
+    evt.target.parentNode.remove();
+};
+
+function crossOffItem(evt) {
+    evt.preventDefault();
+    evt.target.classList.toggle("checked");
+
+    if(evt.target.classList.contains("checked") === true) {
+        alert("Job well done! Item has been crossed off.");
+    } else {
+        alert("Item has been added back.");
+    };
+};

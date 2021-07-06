c79f84926801a936b4fdb8235cb104716662d1b5
carterhanks <carterhanks@gmail.com>
2021-07-05 19:24:14 -0600

    Adding Alerts - Needs Timer?

--- public/index.js
+++ public/index.js
@@ -4,15 +4,39 @@ let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
 addToDoButton.addEventListener('click', function(){
-    var paragraph = document.createElement('li');
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
+    let item = document.createElement('li');
+
+    item.classList.add('paragraph-styling');
+    item.innerText = inputField.value;
+    toDoContainer.appendChild(item);
     inputField.value = "";
-    paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = "line-through";
+
+    alert("Item Added!");
+
+    item.addEventListener('click', function(){
+        item.style.textDecoration = "line-through";
+        alert("Item Completed!");
     });
-    paragraph.addEventListener('dblclick', function(){
-        toDoContainer.removeChild(paragraph);
+
+    item.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(item);
+        alert("Item Removed!");
     });
 });
+
+// document.querySelector("input").addEventListener("submit", addItem) 
+// function addItem(evt) {
+//     evt.preventDefault() {
+//         let inputField = document.querySelector("input");
+
+//         const item = document.createElement("li");
+
+//         const itemName = document.createElement("span");
+//             itemName.textContent = inputField.value;
+//             item.appendChild(itemName);
+
+//         const list = document.querySelector("ul");
+//             list.appendChild(item);
+//             inputField.value = ''
+//     }
+// }
